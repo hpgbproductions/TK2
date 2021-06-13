66352113eaa461ae58734077f1783838e0754aa1
hpgbproductions <36079650+hpgbproductions@users.noreply.github.com>
2021-06-13 17:22:10 +0800

    Add files via upload

--- Scripts/AceRadar/AceRadarBackend.cs
+++ Scripts/AceRadar/AceRadarBackend.cs
@@ -199,7 +199,7 @@ public class AceRadarBackend : MonoBehaviour
                 output = new Color(1f, 1f, 1f, 0.5f);
                 break;
             case AceRadarColors.Red:
-                output = new Color(1f, 0f, 0f, 0.5f);
+                output = new Color(1f, 0.1f, 0.1f, 0.5f);
                 break;
             case AceRadarColors.Blue:
                 output = new Color(0f, 0.5f, 1f, 0.5f);
